7ad3c9d7be61375dc84b914f7cbde5f8f4e02b13
Jamond Turner <jamondturner@yahoo.com>
2022-03-27 21:56:54 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+
+
+var customerName = "bob";
+function upperCaseCustomerName(){
+     customerName.toUpperCase;
+     console.log(upperCaseCustomerName);
+}
+
+function setBestCustomer(){
+var BestCustomer = "not bob";
+console.log(BestCustomer);
+}
+
+function overwriteBestCustomer(){
+var bestCustomer = "maybe bob";
+console.log(bestCustomer);
+}
+
+const leastFavoriteCustomer = "karen"
+function changeLeastFavoriteCustomer(){
+var leastFavoriteCustomer = "mike";
+
+}
